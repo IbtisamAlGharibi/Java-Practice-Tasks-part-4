281f7627e151dd53a012d7a25c48a93c076cb793
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-08 16:13:39 +0400

    Task 24: centeredAverage

--- src/Task24.java
+++ src/Task24.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Task24 {
-}
+    public static void main(String[] args) {
+       int [] array = {1,2,3,4,5};
+
+        int result = centeredAverage(array);
+        System.out.println( result);
+    }
+    }
+
+    public static int centeredAverage(int[] nums) {
+
+        int sum = 0;
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        sum = sum - min - max;
+
+        int average = sum / (nums.length - 2);
+
+        return average;
+    }
+
+
