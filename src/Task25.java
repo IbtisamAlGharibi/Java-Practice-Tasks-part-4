5e5607e9ddc72549c2b653ec1f8101c119d521e4
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-09 10:04:04 +0400

    Task 25: has22

--- src/Task25.java
+++ src/Task25.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Task25 {
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5};
+        boolean result = has22(array);
+        System.out.println(result);
+
+    }
+
+    public static boolean  has22(int[] array){
+        for (int i = 0 ; i<array.length-1 ; i++){
+            if ((array[i] ==2) && (array[i+1] ==2)){
+                return true;
+
+            }
+
+        }
+        return false;
+    }
 }
